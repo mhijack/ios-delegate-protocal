29f131e2516fc49799ba25d0e041d9a88fc448d5
Jianyuan Chen (Jack) <jianyuan.chen12@gmail.com>
2019-03-11 23:20:50 -0700

    Initial Commit

---
+++ Delegate Protocal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Delegate Protocal
+//
+//  Created by jianyuan chen on 2019-03-11.
+//  Copyright © 2019 Jianyuan Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
